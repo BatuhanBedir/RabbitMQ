dee8d0d2d4b0b077e477b7c908aac219dea5d801
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-16 17:37:08 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Publisher;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+
+        factory.Uri = new Uri("amqps://dqbsddti:ma5R1xsLza84BdJZR_p3kx7OG4lq_uIS@woodpecker.rmq.cloudamqp.com/dqbsddti");
+
+        using var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        channel.QueueDeclare("hello-queue", true, false, false);
+
+        string message = "Hello World!";
+
+        var messageBody = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+        Console.WriteLine("Mesaj gönderilmiştir.");
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Subscriber;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://dqbsddti:ma5R1xsLza84BdJZR_p3kx7OG4lq_uIS@woodpecker.rmq.cloudamqp.com/dqbsddti");
+
+        using var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        channel.BasicConsume("hello-queue", true, consumer);
+
+        consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            Console.WriteLine("Gelen Mesaj: " + message);
+        };
+
+        Console.ReadLine();
+    }
+
+}
